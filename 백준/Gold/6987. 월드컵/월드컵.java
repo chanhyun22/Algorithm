fe82b7eb9d4c18e61fe47550750cb76eec4e30c3
Chanhyun Byun <114146654+chanhyun22@users.noreply.github.com>
2025-02-04 15:00:41 +0900

    [Gold IV] Title: 월드컵, Time: 68 ms, Memory: 11648 KB -BaekjoonHub

--- 백준/Gold/6987. 월드컵/월드컵.java
+++ 백준/Gold/6987. 월드컵/월드컵.java
@@ -6,76 +6,78 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int[][] arr;
-	static int ans;
+    
+    static int[][] arr; // 입력받은 경기 결과
+    static boolean valid; // 결과가 가능한지 여부
+    static int[][] matches = { {0,1}, {0,2}, {0,3}, {0,4}, {0,5},
+                               {1,2}, {1,3}, {1,4}, {1,5},
+                               {2,3}, {2,4}, {2,5},
+                               {3,4}, {3,5},
+                               {4,5} }; // 경기 조합 (총 15경기)
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		for (int i = 0; i < 4; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			
-			ans = 0;
-			arr = new int[6][3];
-			int[][] score = new int[6][3];
+        for (int t = 0; t < 4; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-			for (int j = 0; j < 6; j++) {
-				for (int k = 0; k < 3; k++) {
-					arr[j][k] = Integer.parseInt(st.nextToken());
-				}
-			}
+            arr = new int[6][3]; // 각 팀의 승/무/패 기록
 
-			dfs(0, 1, score);
-			
-			bw.write(ans+" ");
-		}
+            int totalGames = 0; // 입력된 총 경기 수 체크 (빠른 탈출 조건)
+            for (int i = 0; i < 6; i++) {
+                for (int j = 0; j < 3; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                    totalGames += arr[i][j];
+                }
+            }
 
-		br.close();
-		bw.close();
-	}
+            // 월드컵 조별 예선의 경기 수는 정확히 15경기여야 함
+            if (totalGames != 30) {
+                bw.write("0 ");
+                continue;
+            }
 
-	public static void dfs(int a, int b, int[][] score) {
-		if (b == 6) {
-			a += 1;
-			b = a + 1;
-		}
+            valid = false; // 가능 여부 플래그
+            dfs(0); // 15경기 중 첫 번째 경기부터 탐색
+            bw.write((valid ? 1 : 0) + " ");
+        }
 
-		if (a == 5) {
-			boolean check = true;
-			for (int i=0; i<6; i++) {
-				for(int j=0; j<3; j++) {
-					if (score[i][j] != arr[i][j]) { 
-						check = false;
-					}
-				}
-			}
-			
-			if (check) {
-				ans = 1;
-			}
-			return;
-		}
-		
-		score[a][0] +=1;
-		score[b][2] +=1;
-		dfs(a, b+1, score);
-		score[a][0] -=1;
-		score[b][2] -=1;
-		
-		score[a][1] +=1;
-		score[b][1] +=1;
-		dfs(a, b+1, score);
-		score[a][1] -=1;
-		score[b][1] -=1;
-		
-		score[a][2] +=1;
-		score[b][0] +=1;
-		dfs(a, b+1, score);
-		score[a][2] -=1;
-		score[b][0] -=1;
+        br.close();
+        bw.close();
+    }
 
-	}
+    // 백트래킹: 15경기를 돌면서 가능한 경우를 탐색
+    public static void dfs(int game) {
+        if (valid) return; // 이미 가능한 경우가 확인되었으면 더 탐색할 필요 없음
 
+        if (game == 15) { // 15경기를 모두 탐색했을 때
+            valid = true;
+            return;
+        }
+
+        int teamA = matches[game][0];
+        int teamB = matches[game][1];
+
+        // 승리: A 승리, B 패배
+        if (arr[teamA][0] > 0 && arr[teamB][2] > 0) {
+            arr[teamA][0]--; arr[teamB][2]--;
+            dfs(game + 1);
+            arr[teamA][0]++; arr[teamB][2]++;
+        }
+
+        // 무승부: A 무승부, B 무승부
+        if (arr[teamA][1] > 0 && arr[teamB][1] > 0) {
+            arr[teamA][1]--; arr[teamB][1]--;
+            dfs(game + 1);
+            arr[teamA][1]++; arr[teamB][1]++;
+        }
+
+        // 패배: A 패배, B 승리
+        if (arr[teamA][2] > 0 && arr[teamB][0] > 0) {
+            arr[teamA][2]--; arr[teamB][0]--;
+            dfs(game + 1);
+            arr[teamA][2]++; arr[teamB][0]++;
+        }
+    }
 }
