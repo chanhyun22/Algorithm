ab2bf4a400db5343846cb923adbbe4c36145ffc0
Chanhyun Byun <114146654+chanhyun22@users.noreply.github.com>
2025-02-06 02:05:28 +0900

    [Gold II] Title: 상어 중학교, Time: 112 ms, Memory: 16012 KB -BaekjoonHub

--- 백준/Gold/21609. 상어 중학교/상어 중학교.java
+++ 백준/Gold/21609. 상어 중학교/상어 중학교.java
@@ -1,249 +1,171 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static int N, M, ans;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+    static List<Point> rainbow;
+    static PriorityQueue<Group> pq;
 
-	static int N, M;
-	static int ans;
-	static int[][] map;
-	static boolean[][] visited;
-	static int[] dr = { -1, 1, 0, 0 };
-	static int[] dc = { 0, 0, -1, 1 };
-	static List<Point> rainbow;
-	static PriorityQueue<Group> pq;
+    public static class Point {
+        int r, c;
+        Point(int r, int c) { this.r = r; this.c = c; }
+    }
 
-	public static class Point {
-		int r;
-		int c;
+    public static class Group implements Comparable<Group> {
+        int count, rcount, r, c;
+        public Group(int count, int rcount, int r, int c) {
+            this.count = count;
+            this.rcount = rcount;
+            this.r = r;
+            this.c = c;
+        }
 
-		Point(int r, int c) {
-			this.r = r;
-			this.c = c;
-		}
-	}
+        @Override
+        public int compareTo(Group o) {
+            if (this.count != o.count) return o.count - this.count;
+            if (this.rcount != o.rcount) return o.rcount - this.rcount;
+            if (this.r != o.r) return o.r - this.r;
+            return o.c - this.c;
+        }
+    }
 
-	public static class Group implements Comparable<Group> {
-		int count;
-		int rcount;
-		int r;
-		int c;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		public Group(int count, int rcount, int r, int c) {
-			this.count = count;
-			this.rcount = rcount;
-			this.r = r;
-			this.c = c;
-		}
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-		@Override
-		public int compareTo(Group o) {
-			if (this.count != o.count) {
-				return o.count - this.count;
-			}
-			if (this.rcount != o.rcount) {
-				return o.rcount - this.rcount;
-			}
-			if (this.r != o.r) {
-				return o.r - this.r;
-			}
-			return o.c - this.c;
-		}
-	}
+        map = new int[N][N];
+        rainbow = new ArrayList<>();
+        ans = 0;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 0) {
+                    rainbow.add(new Point(i, j));
+                }
+            }
+        }
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+        while (true) {
+            visited = new boolean[N][N];
+            pq = new PriorityQueue<>();
 
-		map = new int[N][N];
-		rainbow = new ArrayList<>();
-		ans = 0;
+            for (int r = 0; r < N; r++) {
+                for (int c = 0; c < N; c++) {
+                    if (!visited[r][c] && map[r][c] > 0) {
+                        for (Point p : rainbow) visited[p.r][p.c] = false; // 무지개 블록 방문 초기화
 
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				if (map[i][j] == 0) {
-					rainbow.add(new Point(i, j));
-				}
-			}
-		}
+                        int color = map[r][c];
+                        int count = 0, rcount = 0;
+                        Queue<Point> q = new LinkedList<>();
+                        q.add(new Point(r, c));
+                        visited[r][c] = true;
 
-		while (true) {
+                        while (!q.isEmpty()) {
+                            Point p = q.poll();
+                            count++;
+                            if (map[p.r][p.c] == 0) rcount++;
 
-			visited = new boolean[N][N];
-			pq = new PriorityQueue<>();
+                            for (int d = 0; d < 4; d++) {
+                                int nr = p.r + dr[d], nc = p.c + dc[d];
+                                if (isIn(nr, nc) && !visited[nr][nc] && (map[nr][nc] == color || map[nr][nc] == 0)) {
+                                    visited[nr][nc] = true;
+                                    q.add(new Point(nr, nc));
+                                }
+                            }
+                        }
 
-			for (int r = 0; r < N; r++) {
-				for (int c = 0; c < N; c++) {
-					if (!visited[r][c] && map[r][c] != 0 && map[r][c] != -1 && map[r][c] != -2) {
-						for (Point p : rainbow) {
-							visited[p.r][p.c] = false;
-						}
+                        if (count >= 2) pq.add(new Group(count, rcount, r, c));
+                    }
+                }
+            }
 
-						int color = map[r][c];
-						visited[r][c] = true;
-						int count = 0;
-						int rcount = 0;
-						Queue<Point> q = new LinkedList<>();
-						q.add(new Point(r, c));
+            if (!pq.isEmpty()) {
+                Group g = pq.poll();
+                remove(g.r, g.c, map[g.r][g.c]);
+            } else {
+                break;
+            }
 
-						while (!q.isEmpty()) {
-							Point p = q.poll();
-							count++;
-							if (map[p.r][p.c] == 0) {
-								rcount++;
-							}
+            gravity();
+            rotate();
+            gravity();
 
-							for (int d = 0; d < 4; d++) {
-								if (isIn(p.r + dr[d], p.c + dc[d]) && !visited[p.r + dr[d]][p.c + dc[d]]) {
-									if (map[p.r + dr[d]][p.c + dc[d]] == color || map[p.r + dr[d]][p.c + dc[d]] == 0) {
-										visited[p.r + dr[d]][p.c + dc[d]] = true;
-										q.add(new Point(p.r + dr[d], p.c + dc[d]));
-									}
-								}
-							}
+            // 무지개 블록 갱신
+            rainbow.clear();
+            for (int r = 0; r < N; r++) {
+                for (int c = 0; c < N; c++) {
+                    if (map[r][c] == 0) rainbow.add(new Point(r, c));
+                }
+            }
+        }
 
-						}
+        bw.write(ans + "\n");
+        br.close();
+        bw.close();
+    }
 
-						if (count >= 2) {
-							pq.add(new Group(count, rcount, r, c));
-						}
-					}
-				}
-			}
+    private static void rotate() {
+        int[][] tmp = new int[N][N];
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                tmp[N - c - 1][r] = map[r][c]; // 90도 반시계 회전
+            }
+        }
+        map = tmp;
+    }
 
-			if (!pq.isEmpty()) {
-				Group g = pq.poll();
+    private static void gravity() {
+        for (int c = 0; c < N; c++) {
+            int bottom = N - 1;
+            for (int r = N - 1; r >= 0; r--) {
+                if (map[r][c] == -1) {
+                    bottom = r - 1;
+                } else if (map[r][c] >= 0) {
+                    if (bottom != r) {
+                        map[bottom][c] = map[r][c];
+                        map[r][c] = -2;
+                    }
+                    bottom--;
+                }
+            }
+        }
+    }
 
-				int color = map[g.r][g.c];
-				remove(g.r, g.c, color);
+    private static void remove(int r, int c, int color) {
+        Queue<Point> q = new LinkedList<>();
+        boolean[][] visited = new boolean[N][N];
+        q.add(new Point(r, c));
+        visited[r][c] = true;
+        map[r][c] = -2;
+        int count = 1;
 
-			} else {
-				break;
-			}
+        while (!q.isEmpty()) {
+            Point p = q.poll();
+            for (int d = 0; d < 4; d++) {
+                int nr = p.r + dr[d], nc = p.c + dc[d];
+                if (isIn(nr, nc) && !visited[nr][nc] && (map[nr][nc] == color || map[nr][nc] == 0)) {
+                    visited[nr][nc] = true;
+                    map[nr][nc] = -2;
+                    q.add(new Point(nr, nc));
+                    count++;
+                }
+            }
+        }
 
-			gravity();
+        ans += count * count;
+    }
 
-			rotate();
-
-			gravity();
-
-			rainbow = new ArrayList<>();
-			for (int r = 0; r < N; r++) {
-				for (int c = 0; c < N; c++) {
-					if (map[r][c] == 0) {
-						rainbow.add(new Point(r, c));
-					}
-				}
-			}
-
-		}
-
-		bw.write(ans + "");
-
-		br.close();
-		bw.close();
-	}
-
-	private static void rotate() {
-
-		int[][] tmp = new int[N][N];
-
-		for (int r = 0; r < N; r++) {
-			for (int c = 0; c < N; c++) {
-				tmp[r][c] = map[c][N - r - 1];
-			}
-		}
-
-		for (int r = 0; r < N; r++) {
-			for (int c = 0; c < N; c++) {
-				map[r][c] = tmp[r][c];
-			}
-		}
-
-	}
-
-	private static void gravity() {
-
-		int[][] tmp = new int[N][N];
-
-		for (int r = 0; r < N; r++) {
-			for (int c = 0; c < N; c++) {
-				tmp[r][c] = -2;
-			}
-		}
-
-		for (int c = 0; c < N; c++) {
-			int count = 0;
-			for (int r = N - 1; r >= 0; r--) {
-				if (map[r][c] == -2) {
-					count++;
-					continue;
-				}
-
-				if (map[r][c] == -1) {
-					tmp[r][c] = -1;
-					count = 0;
-					continue;
-				}
-
-				tmp[r + count][c] = map[r][c];
-			}
-		}
-
-		for (int r = 0; r < N; r++) {
-			for (int c = 0; c < N; c++) {
-				map[r][c] = tmp[r][c];
-			}
-		}
-
-	}
-
-	private static void remove(int r, int c, int color) {
-
-		Queue<Point> q = new LinkedList<>();
-
-		map[r][c] = -2;
-		int count = 0;
-		q.add(new Point(r, c));
-
-		while (!q.isEmpty()) {
-			Point p = q.poll();
-			count++;
-
-			for (int d = 0; d < 4; d++) {
-				if (isIn(p.r + dr[d], p.c + dc[d])) {
-					if (map[p.r + dr[d]][p.c + dc[d]] == color || map[p.r + dr[d]][p.c + dc[d]] == 0) {
-						map[p.r + dr[d]][p.c + dc[d]] = -2;
-						q.add(new Point(p.r + dr[d], p.c + dc[d]));
-					}
-				}
-			}
-
-		}
-
-		ans += (int) Math.pow(count, 2);
-	}
-
-	private static boolean isIn(int i, int j) {
-		if (i >= 0 && i < N && j >= 0 && j < N) {
-			return true;
-		}
-
-		return false;
-	}
-
+    private static boolean isIn(int r, int c) {
+        return r >= 0 && r < N && c >= 0 && c < N;
+    }
 }
